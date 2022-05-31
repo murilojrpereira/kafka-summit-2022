987be5d59d6aaf0860abcecc347830f396c80653
Murilo Junio Rezende Pereira <murilojrpereira@gmail.com>
2022-06-01 00:10:25 +0200

    Kafka Examples Kafka Summit

---
+++ spring-kafka-app6/src/test/java/app6/SpringKafkaApp6Tests.java
@@ -0,0 +1,13 @@
+package app6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApp6Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
